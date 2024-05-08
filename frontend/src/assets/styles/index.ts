56d4e6afb6ceee74016e76684a90f71b254edcf8
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-05-08 14:35:04 -0300

    Recriando a header do projeto frontend

--- frontend/src/assets/styles/index.ts
+++ frontend/src/assets/styles/index.ts
@@ -1,5 +1,6 @@
-// IMPORT CSS
-import './css/global.css';
-
-// IMPORT BOOTSTRAP
 import './bootstrap/bootstrap.min.css';
+
+import './css/reset.css';
+import './css/global.css';
+import './css/animated.css';
+import './css/owl.css';
